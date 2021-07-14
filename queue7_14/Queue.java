eb760d7b7b79c984e14066d5f9cac0b689847e48
Dragon-yjl <2286920873@qq.com>
2021-07-14 23:44:08 +0800

    实现队列接口

---
+++ queue7_14/Queue.java
@@ -0,0 +1,56 @@
+package queue7_14;
+
+
+//仿造 真正的（java.util.Queue） 的Queue接口
+
+import java.util.NoSuchElementException;
+
+public interface Queue {
+    //第一组方法/
+    //通过抛出异常 通知错误
+
+    //插入
+    //永远返回true
+    default boolean add(Integer e) {
+        if(offer(e) == false) {
+            throw new IllegalStateException();
+        }
+        return true;
+    }
+
+    //j检索队首元素，但是不删除
+    default Integer element() {
+        Integer e = peek();
+        if(e == null) {
+            throw new  NoSuchElementException();
+        }
+        return e;
+    }
+
+    //返回并删除队首元素
+    default Integer remove() {
+        Integer e = poll();
+        if(e == null) {
+            throw new  NoSuchElementException();
+        }
+        return e;
+    }
+
+
+    //第二组方法
+    //通过特殊的返回值，通知错误
+
+    //插入
+    //永远返回true
+    boolean offer(Integer e);
+
+    //看队首元素 但不删除
+    //如果返回null 代表队列是空的
+    Integer peek();
+
+
+    //返回并删除队首元素
+    //如果返回null 代表队列是空的
+    Integer poll();
+
+}
